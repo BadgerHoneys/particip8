28b069b396c2b38ccd47e047ff9d0ac81f2a458c
salinn <pmd6624@rit.edu>
2015-10-04 16:58:25 -0400

    evaluation type created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :evaluation_types, except: [:new, :edit]
   resources :school_classes, except: [:new, :edit]
   resources :teachers, except: [:new, :edit]
   resources :students, except: [:new, :edit]
