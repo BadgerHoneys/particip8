84ba12250d5ffb6598ef363f0306247f2fc03ab9
salinn <pmd6624@rit.edu>
2015-10-05 00:14:19 -0400

    updated seeds file load

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,15 @@ User.create(first_name: "Zach", last_name: "Downs", email: "zxd4321@rit.edu")
 
 #No depends
 load 'db/seeds/districts.rb'
-load 'db/seeds/evaluation_types'
-load 'db/seeds/rating_types'
+load 'db/seeds/evaluation_types.rb'
+load 'db/seeds/rating_types.rb'
 
 
 #Depends
-load 'db/seeds/schools' #depends on districts
-load 'db/seeds/evaluation_templates' #depends on school_class, rating_type, and evaluation_type
-load 'db/seeds/evaluations' #depends on evaluations template
-load 'db/seeds/ratings' #depends on evaluation and student
-load 'db/seeds/school_classes' #depends on school and teacher
-load 'db/seeds/students' #depends on school
+load 'db/seeds/schools.rb' #depends on districts
+load 'db/seeds/students.rb' #depends on school
+load 'db/seeds/teachers.rb' #depends on school
+load 'db/seeds/school_classes.rb' #depends on school and teacher
+load 'db/seeds/evaluation_templates.rb' #depends on school_class, rating_type, and evaluation_type
+load 'db/seeds/evaluations.rb' #depends on evaluations template
+load 'db/seeds/ratings.rb' #depends on evaluation and student
