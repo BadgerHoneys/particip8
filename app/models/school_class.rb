0981e205c26ba1e98d9b9508540e9957c92a1fcc
salinn <pmd6624@rit.edu>
2015-10-21 16:18:05 -0400

    updated evaltuation template for deleting

--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -2,7 +2,7 @@ class SchoolClass < ActiveRecord::Base
   belongs_to :school
   belongs_to :teacher
 
-  has_many :evaluation_templates
+  has_many :evaluation_templates, dependent: :destroy
   has_and_belongs_to_many :students
   has_and_belongs_to_many :teachers
 end
