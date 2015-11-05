76efe1a45a39b81b12a2624b474428255fdd6a9f
salinn <pmd6624@rit.edu>
2015-11-04 20:07:10 -0500

    creates a rating type and evaluation template for each class when a class is made

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,5 +1,4 @@
 class Evaluation < ActiveRecord::Base
   belongs_to :evaluation_template
   has_many :ratings
-
 end
--- app/models/school_class.rb
+++ app/models/school_class.rb
@@ -5,4 +5,11 @@ class SchoolClass < ActiveRecord::Base
   has_many :evaluation_templates, dependent: :destroy
   has_and_belongs_to_many :students
   has_and_belongs_to_many :teachers
+
+  after_create :create_attendance_eval
+
+  def create_attendance_eval
+    rating_type = RatingType.create!(name: 'Attendance', scale: '0-1')
+    EvaluationTemplate.create!(name: 'Attendance', school_class_id: id, rating_type_id: rating_type.id)
+  end
 end
