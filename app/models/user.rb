2156f356c587dc514cb1e08b776c040b080fb0a6
Josh Woodward <jdw6359@rit.edu>
2015-11-30 17:24:51 -0500

    Complete password reset feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,25 @@
 class User < ActiveRecord::Base
-  before_save :encrypt_password
+  before_create :encrypt_password
+  
   
   has_many :attendances, through: :current_days
 
+  def to_json(options={})
+    options[:except] ||= [:password, :password_reset_token, :salt]
+    super(options)
+  end
+
   def encrypt_password
+    binding.pry
     if password.present?
       self.salt = BCrypt::Engine.generate_salt
       self.password = BCrypt::Engine.hash_secret(password, salt)
     end
+  end
+
+  def generate_password_reset_token!
+    #generate reset token here
+    update_attribute(:password_reset_token, SecureRandom.urlsafe_base64(48))
   end
 
 	def generate_auth_token
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   resources :attendances, except: [:new, :edit]
   resources :current_days, except: [:new, :edit]
   resources :ratings, except: [:new, :edit]
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
 
   resources :evaluation_templates, except: [:new, :edit]
   resources :rating_types, except: [:new, :edit]
+
+  resources :password_reset, only: [:show, :create, :update]
 
   get 'school_classes/available' => 'school_classes#available'
   resources :school_classes, except: [:new, :edit] do
@@ -32,9 +35,6 @@ Rails.application.routes.draw do
 
   post 'email_verification/generate_token' => 'email_verification#generate_token'
   post 'email_verification/verify_token' => 'email_verification#verify_token'
-
-  post 'password_reset/generate_token' => 'password_reset#generate_token'
-  post 'password_reset/verify_token' => 'password_reset#verify_token'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
